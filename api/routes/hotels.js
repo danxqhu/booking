d4d848b4d6a7d951b4127a3b79726ad6b3c60a69
xinqiang-hu <danxqhu@gmail.com>
2022-09-08 17:47:44 +0800

    update api

---
+++ api/routes/hotels.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import Hotel from '../models/Hotel.js';
+
+const router = express.Router();
+
+// CREATE
+router.post('/', async (req, res) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// UPDATE
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE
+// GET
+// GET ALL
+
+router.get('/register', (req, res) => {
+  res.send('Hello, this is auth register endpoint');
+});
+
+export default router;

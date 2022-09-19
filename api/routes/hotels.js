0d918f4a49cb4dc2d9f8b2facef6da05079da35a
Daniel <danxqhu@gmail.com>
2022-09-19 23:05:34 +0800

    finish users

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,17 +2,18 @@ import express from 'express';
 import Hotel from '../models/Hotel.js';
 import { createError } from '../utils/error.js';
 import { createHotel, updateHotel, deleteHotel, getHotel, getHotels } from '../controllers/hotel.js';
+import { verifyToken, verifyUser, verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 // CREATE
-router.post('/', createHotel);
+router.post('/', verifyAdmin, createHotel);
 
 // UPDATE
-router.put('/:id', updateHotel);
+router.put('/:id', verifyAdmin, updateHotel);
 
 // DELETE
-router.delete('/:id', deleteHotel);
+router.delete('/:id', verifyAdmin, deleteHotel);
 
 // GET
 router.get('/:id', getHotel);
@@ -20,8 +21,8 @@ router.get('/:id', getHotel);
 // GET ALL
 router.get('/', getHotels);
 
-router.get('/register', (req, res) => {
-  res.send('Hello, this is auth register endpoint');
-});
+// router.get('/register', (req, res) => {
+//   res.send('Hello, this is auth register endpoint');
+// });
 
 export default router;

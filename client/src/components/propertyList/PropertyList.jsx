ad5d489ae0d96c8345dabb35250ac32e70eb1877
xinqiang-hu <danxqhu@gmail.com>
2022-10-09 17:51:51 +0800

    add photos

---
+++ client/src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './propertyList.scss';
+
+export default function PropertyList() {
+  return <div>PropertyList</div>;
+}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import Featured from '../../components/featured/Featured';
 import './home.scss';
 
 export default function Home() {
@@ -8,6 +9,10 @@ export default function Home() {
     <div>
       <Navbar></Navbar>
       <Header></Header>
+      <div className="homeContainer">
+        <Featured></Featured>
+        <h1 className="homeTitle">Browse by property type</h1>
+      </div>
     </div>
   );
 }

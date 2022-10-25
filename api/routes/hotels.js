0874cf2ba0b2a9327beede6adae8759e5cc0d92e
xinqiang-hu <danxqhu@gmail.com>
2022-10-25 11:18:28 +0800

    update backend

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import Hotel from '../models/Hotel.js';
 import { createError } from '../utils/error.js';
-import { createHotel, updateHotel, deleteHotel, getHotel, getHotels } from '../controllers/hotel.js';
+import { createHotel, updateHotel, deleteHotel, getHotel, getHotels, countByCity } from '../controllers/hotel.js';
 import { verifyToken, verifyUser, verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
@@ -20,6 +20,8 @@ router.get('/:id', getHotel);
 
 // GET ALL
 router.get('/', getHotels);
+router.get('/countByCity', countByCity);
+router.get('/countByType', getHotels);
 
 // router.get('/register', (req, res) => {
 //   res.send('Hello, this is auth register endpoint');

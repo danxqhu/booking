2a32ac219ce08e63907c0ea1d558b07aa17c78dd
xinqiang-hu <danxqhu@gmail.com>
2022-12-05 17:54:49 +0800

    update new things

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,9 +1,17 @@
+import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
+import './app.css';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div>
       <Topbar></Topbar>
+      <div className="container">
+        <Sidebar></Sidebar>
+        <Home></Home>
+        {/* <div className="others">other pages</div> */}
+      </div>
     </div>
   );
 }

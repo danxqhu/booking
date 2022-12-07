e39c78a10aaf8e85eee5556a8d5a10eb4838880f
xinqiang-hu <danxqhu@gmail.com>
2022-12-07 17:34:57 +0800

    update

--- admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './widgetSm.css';
+import { Visibility } from '@material-ui/icons';
 
 export default function WidgetSm() {
   return (
@@ -16,7 +17,70 @@ export default function WidgetSm() {
             <span className="widgetSmUsername">Anna Keller</span>
             <span className="widgetSmUserTitle">Software Engineer</span>
           </div>
-          <bbutton className="widgetSmButton"></bbutton>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"></Visibility>
+            Display
+          </button>
+        </li>
+        <li className="widgetSmIListItem">
+          <img
+            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"></Visibility>
+            Display
+          </button>
+        </li>
+        <li className="widgetSmIListItem">
+          <img
+            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"></Visibility>
+            Display
+          </button>
+        </li>
+        <li className="widgetSmIListItem">
+          <img
+            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"></Visibility>
+            Display
+          </button>
+        </li>
+        <li className="widgetSmIListItem">
+          <img
+            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon"></Visibility>
+            Display
+          </button>
         </li>
       </ul>
     </div>
